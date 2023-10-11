86d65ce9f505c575730f8e35ca64d978451bcb91
ItsMeMaui <119463629+ItsMeMaui@users.noreply.github.com>
2023-10-12 04:24:26 +0800

    deleting unneccessary files

--- PMS/admin/components/navbar.php
+++ PMS/admin/components/navbar.php
@@ -43,9 +43,7 @@
           <li>
             <a href="../pages/positions.php" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Positions</a>
           </li>
-          <li>
-            <a href="../pages/deductions.php" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Deductions</a>
-          </li>
+         
           <hr class="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700">
 
           <li>
